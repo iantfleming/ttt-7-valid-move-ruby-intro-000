3f3b96f82ef93869a4ea8757a75980a59a1c5042
Ian Fleming <iantfleming1992@gmail.com>
2020-10-20 21:00:00 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,8 @@
 # code your #valid_move? method here
+def valid_move?(board, input)
+  input.to_i.between?(1,9) && position_taken?(board, input.to_i-1)
 
+<<<<<<< HEAD
 def valid_move?(board, number_entered)
   number_entered.between?(0, 8) && !(position_taken?(board, number_entered))
 end
@@ -15,3 +18,16 @@ def position_taken?(board, index)
 end
 
 
+=======
+end
+  def position_taken?(board, index)
+    if board[index] == " " or board[index] == ""
+      return false
+    elsif board[index] == nil
+      return false    
+    else board[index] == "X" or board[index] == "0"
+      return true
+    end
+  end
+end
+>>>>>>> 943953ca861bdf3b32832fd5239306317f978610
